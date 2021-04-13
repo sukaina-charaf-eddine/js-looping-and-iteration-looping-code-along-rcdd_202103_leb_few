8e940fb5ca3c0faf4d3afd5edfc6e16c7e720b19
Sukaina Charaf Eddine <sukaina.kabalan01@gmail.com>
2021-04-13 08:39:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(x,y){
+  const card = [];
+  for (let i = 0; i < x.length; i++) {
+    card.push(`Thank you, ${x[i]}, for the wonderful ${y} gift!`);
+  }
+  return card;
+}
+ const writeCard = ['Lisa', 'Kaitlin', 'Jan'];
+ writeCards(writeCard,"evnet");
+
+ function countDown(num){
+  if (num >= 0){
+    while (num >= 0){
+      console.log(num);
+      num--;
+    }
+  }
+  else{
+    console.log("Please enter a positive integer!")
+  }
+}
