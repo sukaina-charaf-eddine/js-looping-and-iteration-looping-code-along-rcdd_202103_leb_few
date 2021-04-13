dcfeec629f7cfb0cb547cb1754c6c0a3f0d582fd
Sukaina Charaf Eddine <sukaina.kabalan01@gmail.com>
2021-04-13 08:37:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+//const writeCards=[["Ada", "Brendan", "Ali"], "birthday"];
+
+
+//  for (let i = 0; i < writeCards.length; i++) {
+  //  console.log(`Thank you, ${writeCards[[i]]}, for the wonderful ${writeCards[i]} gift!" `);
+//  }
+
+//  return writeCards;
+//}
+
+//greetings(writeCards);
+const writeCard = [["Ada", "Brendan", "Ali"], "birthday"];
+writeCards(writeCard,"birthday");
+function writeCards(x,y){
+  const birthday = [];
+  for (let i = 0; i < x[0].length; i++) {
+    birthday.push(`Thank you, ${x[0][i]}, for the wonderful ${y} gift!`);
+  }
+  return birthday;
+}
